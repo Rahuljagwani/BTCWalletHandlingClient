4044779cf3b8229458e0aea7ac006ab37f784ff3
Rahuljagwani <rahuljagwani1012@gmail.com>
2023-10-16 06:26:27 +0530

    HashRouter for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Transactions from './Components/Transactions';
 import ListWallets from './Components/ListWallets';
 
